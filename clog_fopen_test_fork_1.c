226c61ea4b6a632f37f3fe28acd1d83b8bb5b7ca
Descacharrado <Luis_koopa@hotmail.com>
2022-06-29 22:38:46 +0200

    prototype I code cleanup

--- clog_fopen_test_fork_1.c
+++ clog_fopen_test_fork_1.c
@@ -3,8 +3,6 @@
 // of the current's version changelog. It must be able to open the file too with the
 // standard text file app.
 
-//#define _CRT_SECURE_NO_WARNINGS
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -13,40 +11,13 @@ int main() {
 
     FILE *clog;
     int buffer;
-    // int foresight[20] = {0};
-    // int major_version = 17;
-    // int minor_version = 5;
     int i,displacement,versions_passed;
-
-    // int previous_minor_version = minor_version-1;
-
-    // Take into account the possibility of the first minor version (e.g. 1.17.0)
-    //if (minor_version == 0) {
-    //    previous_minor_version = 0;
-    //}
-
-    //char str2[20] = {0}; itoa(major_version, str2, 10);
-    //char str4[20] = {0}; itoa(previous_minor_version, str4, 10);
-    //char str3[1] = ".";
-    //char str1[] = "Version 1.";
-
-    //char version_string[100];
-
-    //strcpy(version_string,str1);
-    //strcat(version_string,str2);
-    //strcat(version_string,str3);
-    //// itoa overflows and concatenates str3 with str4
-    ////strcat(version_string,str4);
-
-    //printf("%s\n", version_string);
-    //system("pause");
 
     if ((clog = fopen("changelog.md", "r")) != NULL) {
         versions_passed = 0;
         while ((buffer = getc(clog)) != EOF) {
             fseek(clog, -1, 1);
 
-            // This strongly depends on changelog  formatting
             if (fgetc(clog) == '#') {
                 if (fgetc(clog) == '#') {
                     if (fgetc(clog) != '#') {
@@ -68,7 +39,6 @@ int main() {
                 }
             }
             putc(buffer, stdout);
-            // system("pause");
         }
         fclose(clog);
     }
