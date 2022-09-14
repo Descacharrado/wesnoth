0bfca918662bef4b79f0a6bee2fb1ad9f282d069
Descacharrado <Luis_koopa@hotmail.com>
2022-09-14 21:22:10 +0200

    Add categorization of each line

--- clog_fopen_test_fork_1.c
+++ clog_fopen_test_fork_1.c
@@ -1,7 +1,11 @@
 // Program created by Luis Miguel Iglesias Sánchez in order to test a reading and parsing
 // function that strips unnecessary characters from a file and shows an embellished version
-// of the current's version changelog. It must be able to open the file too with the
+// of the current's version changelog. TODO: It must be able to open the file too with the
 // standard text file app.
+
+
+// TODO: Add a 5 char memory vector on which each category of line is decided
+//      to avoid mistakenly calling a category a PR/Issue Code.
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -12,17 +16,21 @@ int main() {
     FILE *clog;
     int buffer;
     int i,displacement,versions_passed;
+    char firstchar;
 
     if ((clog = fopen("changelog.md", "r")) != NULL) {
         versions_passed = 0;
-        while ((buffer = getc(clog)) != EOF) {
+        while ((firstchar = buffer = getc(clog)) != EOF) {
             fseek(clog, -1, 1);
 
+            // READS 3 CHARACTERS INTO THE FUTURE!
+            // EVERY LOOP READS, AT LEAST! ONE CHARACTER
             if (fgetc(clog) == '#') {
                 if (fgetc(clog) == '#') {
                     if (fgetc(clog) != '#') {
                         fseek(clog, -1, 1);
                         versions_passed++;
+                        printf("(Version)");
 
                         // DEBUG: printf("\n\n %d VERSION PASSED \n\n", versions_passed);
 
@@ -32,11 +40,16 @@ int main() {
                     }
                     else {
                         fseek(clog, -1, 1);
+                        printf("(Category)");
                     }
                 }
                 else {
                     fseek(clog, -1, 1);
+                    printf("(PR/Issue Code)");
                 }
+            }
+            else if (firstchar == '*'){
+                printf("(feature)");
             }
             putc(buffer, stdout);
         }
